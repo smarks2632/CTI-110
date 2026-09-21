1e79c61ddc4cd6c599ae1692105c53dac6db987c
smarks2632 <markss2632@student.faytechcc.edu>
2026-09-20 20:10:58 -0400

    Add files via upload

---
+++ P1LAB1_MarksShinade.py
@@ -0,0 +1,9 @@
+# Shinade Marks
+# 9/15/26
+# p1lab1.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "!", "Welcome to CTI-110!")
